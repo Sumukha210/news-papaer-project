f5051bfcab46c431728c9139d86ab8632eeffd2f
Sumukha210 <sumukhakb@gmail.com>
2020-10-01 12:07:45 +0530

    Updates

---
+++ precache-manifest.482d3d883f3152216b099b513b8aa265.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e09207ce7efdebd059815abc084ab9",
+    "url": "/news-paper-project/index.html"
+  },
+  {
+    "revision": "4017956abfa4d232f845",
+    "url": "/news-paper-project/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "db41c408efa0fbb2d6b8",
+    "url": "/news-paper-project/static/css/main.221745db.chunk.css"
+  },
+  {
+    "revision": "4017956abfa4d232f845",
+    "url": "/news-paper-project/static/js/2.e8be92fa.chunk.js"
+  },
+  {
+    "revision": "2f3b410837970f77906077d0b44a72ca",
+    "url": "/news-paper-project/static/js/2.e8be92fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db41c408efa0fbb2d6b8",
+    "url": "/news-paper-project/static/js/main.5843134c.chunk.js"
+  },
+  {
+    "revision": "d95d23eb334e24707806",
+    "url": "/news-paper-project/static/js/runtime-main.2539db3f.js"
+  },
+  {
+    "revision": "62d8b6ef8e6c796a5ddad822dff96874",
+    "url": "/news-paper-project/static/media/fb.62d8b6ef.svg"
+  },
+  {
+    "revision": "e75317fafc5e835195adf92be513d35b",
+    "url": "/news-paper-project/static/media/insta.e75317fa.svg"
+  },
+  {
+    "revision": "5a71fa48ae1965c00f977c5aeb436b57",
+    "url": "/news-paper-project/static/media/not_found.5a71fa48.jpg"
+  },
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "/news-paper-project/static/media/search-solid.c6a0effb.svg"
+  },
+  {
+    "revision": "a1223de504180a12574a17a1f30c5a08",
+    "url": "/news-paper-project/static/media/twitter.a1223de5.svg"
+  }
+]);
